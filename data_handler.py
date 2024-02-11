7abc2d1f3a547a2cb19aa47c6de8d1c77f6130c2
Whob1 <worriedhob@gmail.com>
2024-02-11 08:45:06 -0800

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -1,14 +1,35 @@
+
 import csv
+import os
 import json
-from utils import DOMAINS_FILE, DATA_FILE
+from tempfile import NamedTemporaryFile
+import shutil
+from utils import DOMAINS_FILE, DATA_FILE, OUTPUT_DIR
 
-def save_to_csv(data, filename=DATA_FILE):
-    with open(filename, mode='a', newline='', encoding='utf-8') as f:
-        writer = csv.DictWriter(f, fieldnames=['url', 'score', 'keywords', 'summarized_text'])
-        if f.tell() == 0:
-            writer.writeheader()
+class DataHandler:
+    def __init__(self):
+        self.temp_files = []
+
+    def save_data_chunk(self, data):
+        temp_file = NamedTemporaryFile(delete=False, dir=OUTPUT_DIR, mode='w', newline='', encoding='utf-8')
+        writer = csv.DictWriter(temp_file, fieldnames=['url', 'text'])
+        writer.writeheader()
         writer.writerows(data)
+        self.temp_files.append(temp_file.name)
+        temp_file.close()
 
-def save_domain_scores(domain_scores):
-    with open(DOMAINS_FILE, 'w') as file:
-        json.dump(domain_scores, file, indent=4)
+    def consolidate_data(self):
+        with open(DATA_FILE, 'w', newline='', encoding='utf-8') as f_out:
+            writer = csv.writer(f_out)
+            writer.writerow(['url', 'text'])  # Write final CSV header
+            for temp_file in self.temp_files:
+                with open(temp_file, 'r', newline='', encoding='utf-8') as f_in:
+                    reader = csv.reader(f_in)
+                    next(reader)  # Skip header
+                    writer.writerows(reader)
+                os.unlink(temp_file)  # Delete temporary file
+        self.temp_files.clear()  # Clear the list of temporary files
+
+    def save_domain_scores(self, domain_scores):
+        with open(DOMAINS_FILE, 'w') as file:
+            json.dump(domain_scores, file, indent=4)
